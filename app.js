6e318eb7e776cb7c29f697d92a7d909a073858af
germansanz93 <germansanz93@gmail.com>
2021-10-15 13:08:06 -0300

    Server creado, listo para agregar rutas

---
+++ app.js
@@ -0,0 +1,16 @@
+//modulos de terceros
+const express = require('express');
+
+//modulos propios
+
+//configs
+const app = express();
+const PORT = 3000;
+
+//rutas
+
+
+//server escuchando
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`)
+})
