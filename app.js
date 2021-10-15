0904a980e72189d1e825a9a2ea9819dcdefdac9b
germansanz93 <germansanz93@gmail.com>
2021-10-15 13:36:52 -0300

    Agregado el path public y las rutas a la homepage y a contacto

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 //modulos de terceros
 const express = require('express');
+const path = require('path');
 
 //modulos propios
 
 //configs
 const app = express();
 const PORT = 3000;
+const publicPath = path.join(__dirname, '/public');
+app.use(express.static(publicPath));
 
 //rutas
 
+//home
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/home.html'));
+})
+
+//contacto
+app.get('/contacto', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/contacto.html'));
+})
 
 //server escuchando
 app.listen(PORT, () => {
