bf9633eed3d4fd8b0283dbed3af44f33a779ce8d
germansanz93 <germansanz93@gmail.com>
2022-02-22 00:29:06 -0300

    port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const loggedUserMiddleware = require('./middleware/loggedUserMiddleware');
 
 //configs
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT | 5000;
 const publicPath = path.join(__dirname, 'public');
 app.use(express.static(publicPath));
 app.use(express.json());
