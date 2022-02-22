6c0d1c42b05a9be5e7aa6e264f1a3cd1a1f15d0b
germansanz93 <germansanz93@gmail.com>
2022-02-22 00:46:49 -0300

    deploy bug

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.use('/', mainRouter)
 
 
 //server escuchando
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running at port ${PORT}`)
+port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`)
 })
